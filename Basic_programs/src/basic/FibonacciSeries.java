ecef3f40a5fffbfe3f40b4a6eddf9902a112df02
Bala-Esakki <balaesakki1@gmail.com>
2025-01-24 11:11:43 +0530

    Fix : Programm written successfully

--- Basic_programs/src/basic/FibonacciSeries.java
+++ Basic_programs/src/basic/FibonacciSeries.java
@@ -1,11 +1,43 @@
 package basic;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
 public class FibonacciSeries {
 	
-	public static void main(String[] args) {
-		System.out.println("Fibonacci series program");
-		System.out.println("continue to write program..");
+	static Scanner s = new Scanner(System.in);
+	
+	public static List fibonacciSeries(int n) {
+		List<Integer> fs = new LinkedList<Integer>();
 		
+		int n1 = 0;
+		int n2 = 1;
+		
+		fs.add(n1);
+		fs.add(n2);
+		
+		int n3 ;
+		
+		for(int i=2;i<n;i++) {
+			n3 = n1+n2;
+			fs.add(n3);
+			n1 = n2;
+			n2 = n3;
+		}
+		
+		return fs;
+	}
+	
+	public static void main(String[] args) {
+		System.out.print("Enter the limit : ");
+		int limit = s.nextInt();
+		
+		List<Integer> ls = fibonacciSeries(limit);
+		
+		for(Integer i: ls) {
+			System.out.print(i+" ");
+		}
 	}
 
 
